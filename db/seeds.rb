1d3eeb6766b056edc2f0fd788b0bd3c1001a8df5
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2020-12-15 15:27:58 +0200

    Added seeds with users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+def create_users
+  puts 'Creating users...'
+
+  @users = FactoryBot.create_list(:user, 25)
+end
+
+puts 'The populating of a database had started!'
+
+create_users
+
+puts 'Done!'
